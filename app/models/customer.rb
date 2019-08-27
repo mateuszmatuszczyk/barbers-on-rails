e77ad0c0564a87f51de86fcf6c88eb8ee13196ed
mateuszmatuszczyk <matim3k@gmail.com>
2019-08-27 03:34:42 +0100

    Fixed custom gem

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  belongs_to :user, index: { unique: true }
+  belongs_to :user
   has_many :appointments, dependent: :destroy
 
   validates :customer_name, presence: true , length: {minimum:3, maximum:30}
